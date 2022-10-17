7f117cb5a43cb7b7468ae5543d7fbae6f9252f23
DE PAZ, IVAN, Vodafone Spain <ivan.depaz@vodafone.com>
2022-10-17 11:17:57 +0200

    feat: initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-angular-interview app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('frontend-angular-interview app is running!');
   });
 });
